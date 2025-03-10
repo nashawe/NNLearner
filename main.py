9e0c2afd9b855e3e0bd0db56c1e2309cdc2c7513
nashawe <nathaniel.shawe@gmail.com>
2025-03-10 21:40:40 +0100

    Final Commit of main

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
 import numpy as np
-<<<<<<< HEAD
- 
- # Define the sigmoid activation function:
- def sigmoid(x):
-     return 1 / (1 + np.exp(-x))
- 
- # Initialize the neuron class and its variables (weights and bias):
- class Neuron:
-     def __init__(self, weights, bias):
-         self.weights = weights
-         self.bias = bias
-     
-     # Define the "feedforward" method for computing the output of the neuron:
-     def feedforward(self, inputs): 
-         # Uses np.dot to calculate dot product of inputs and weights. Adds the bias.
-         total = np.dot(self.weights, inputs) + self.bias
-         sig_val = sigmoid(total)
-         return round(sig_val, 4)
-     
- # Defines the actual values used in this neuron. Weights, bias.
- weights = np.array([0,1,])
- bias = 4
- n = Neuron(weights, bias)
- 
- # Sets the inputs that were determined. These could be any values and any number of values.
- x = np.array([2,3,])
- # Prints the final result after being passed through the sigmoid activation function.
- print(f"When the inputs are {x} and the weights are {weights}, the neuron outputs {n.feedforward(x)}.")
- 
- # Final Explanation: 1) Numpy is imported so we can better do mathematical operations with arrays and equations. 2) We define the sigmoid function with x as our argument (x will be the input later on). 3) We initialize the Neuron class with its respective traits (weights, bias). 4) We define the feedforward function with the arguments (self and inputs). This will produce the dot product of the inputs and then put that value through the sigmoid function. 5) After all of that, the specific values that will be used in the neuron are defined and passed through. 6) The final line means that the final value (in range 0-1) is printed in the console. 
-=======
 
 # Define the sigmoid activation function:
 def sigmoid(x):
@@ -41,8 +10,8 @@ class Neuron:
         self.weights = weights
         self.bias = bias
     
-    # Define the "feedforward" method for computing the output of the neuron:
-    def feedforward(self, x): 
+    # Define the "neuron_func" method for computing the output of the neuron:
+    def neuron_func(self, x): 
         # Uses np.dot to calculate dot product of inputs and weights. Adds the bias.
         total = np.dot(self.weights, x) + self.bias
         sig_val = sigmoid(total)
@@ -66,14 +35,12 @@ class NeuralNetwork:    # Initializes the neural network object.
         self.h2 = Neuron(weights, bias)     # One of the two hidden neurons (h2)
         self.o1 = Neuron(weights, bias)     # The final output neuron (o1). Its inputs will be the outputs of h1 and h2. Check diagram.
     def feedforward(self, x):
-        out_h1 = self.h1.feedforward(x)     # This defines the outputs of 
-        out_h2 = self.h2.feedforward(x)
+        out_h1 = self.h1.neuron_func(x)     # Defines the output of the first hidden neuron. 
+        out_h2 = self.h2.neuron_func(x)     # Defines the output of the second hidden neuron.
 
-        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
-        return out_o1
+        out_o1 = self.o1.neuron_func(np.array([out_h1, out_h2]))    # Defines output of output neuron with out_h1 and out_h2 as the inputs.
+        return out_o1   # Returns the final value we want (after original inputs are put through both the hidden neurons and the final neuron.)
 
 network = NeuralNetwork()
 x = np.array([2,3])
 print(network.feedforward(x))
-
->>>>>>> 3ac0588 (Simple, untrained Neural Network added)
