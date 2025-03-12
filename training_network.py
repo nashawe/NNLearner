a86eabad5cbe48145d18828d429b461b91ef638e
nashawe <nathaniel.shawe@gmail.com>
2025-03-12 23:06:22 +0100

    Bug fixes and __call__ method within Neuron object

--- training_network.py
+++ training_network.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Define the sigmoid activation function:
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
 
 def deriv_sig(x):
     fx = sigmoid(x)
@@ -12,27 +12,31 @@ def mse_loss(y_true, y_pred):
   # y_true and y_pred are numpy arrays of the same length.
   return ((y_true - y_pred) ** 2).mean()
 
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+    
+    # Define the method for computing the output of the neuron:
+    def __call__(self, x):      #__call__ makes it so that the neuron class can be called later on as a function
+        # Uses np.dot to calculate dot product of inputs and weights. Adds the bias.
+        total = np.dot(self.weights, x) + self.bias
+        sig_val = sigmoid(total)
+        return sig_val
+
 # Initialize the neuron class and its variables (weights and bias):
 class NeuralNetwork:
     def __init__(self):
-        #weights:
-        self.w1 = np.random.normal()
-        self.w2 = np.random.normal()
-        self.w3 = np.random.normal()
-        self.w4 = np.random.normal()
-        self.w5 = np.random.normal()
-        self.w6 = np.random.normal()
-
-        # bias:
-        self.b1 = np.random.normal()
-        self.b2 = np.random.normal()
-        self.b3 = np.random.normal()
+        #create each neuron with independent weights and biases
+        self.h1 = Neuron([np.random.normal(), np.random.normal()], np.random.normal())
+        self.h2 = Neuron([np.random.normal(), np.random.normal()], np.random.normal())
+        self.o1 = Neuron([np.random.normal(), np.random.normal()], np.random.normal())
     
     def feedforward(self, x):
-        h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-        h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-        o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
-        return o1
+        out_h1 = self.h1(x)
+        out_h2 = self.h2(x)
+        out_o1 = self.o1([out_h1, out_h2])
+        return out_o1
     
     #function to train network
     def train(self, data, all_y_trues):
@@ -41,60 +45,55 @@ class NeuralNetwork:
 
         for epoch in range(epochs):
             for x, y_true in zip(data, all_y_trues):
-                #the same feedforward as before
-                raw_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
-                h1 = sigmoid(raw_h1)
+                # Forward pass
+                raw_h1 = self.h1(x)
+                raw_h2 = self.h2(x)
+                o1 = self.o1([raw_h1, raw_h2])
 
-                raw_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
-                h2 = sigmoid(raw_h2)
-
-                raw_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
-                o1 = sigmoid(raw_o1)
-            #set the outcome of network to predicted value
+                # Set the outcome of network to predicted value
                 y_pred = o1
 
-                #neuron o1
+                # Neuron o1
                 d_L_d_ypred = -2 * (y_true - y_pred)
-                d_ypred_d_w5 = h1 * deriv_sig(raw_o1)
-                d_ypred_d_w6 = h2 * deriv_sig(raw_o1)
-                d_ypred_d_b3 = deriv_sig(raw_o1)
+                d_ypred_d_w5 = raw_h1 * deriv_sig(o1)
+                d_ypred_d_w6 = raw_h2 * deriv_sig(o1)
+                d_ypred_d_b3 = deriv_sig(o1)
 
-                d_ypred_d_h1 = self.w5 * deriv_sig(raw_o1)
-                d_ypred_d_h2 = self.w6 * deriv_sig(raw_o1)
+                d_ypred_d_h1 = self.o1.weights[0] * deriv_sig(o1)
+                d_ypred_d_h2 = self.o1.weights[1] * deriv_sig(o1)
 
                 # Neuron h1
                 d_h1_d_w1 = x[0] * deriv_sig(raw_h1)
                 d_h1_d_w2 = x[1] * deriv_sig(raw_h1)
                 d_h1_d_b1 = deriv_sig(raw_h1)
 
-                #neuron h2
+                # Neuron h2
                 d_h2_d_w3 = x[0] * deriv_sig(raw_h2)
                 d_h2_d_w4 = x[1] * deriv_sig(raw_h2)
                 d_h2_d_b2 = deriv_sig(raw_h2)
 
-                #update weights and biases
+                # Update weights and biases
+                # Neuron o1
+                self.o1.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_w5
+                self.o1.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_w6
+                self.o1.bias -= learn_rate * d_L_d_ypred * d_ypred_d_b3
 
-                #neuron h1
-                self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
-                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
-                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+                # Neuron h1
+                self.h1.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
+                self.h1.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
+                self.h1.bias -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
 
-                #neuron h2
-                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
-                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
-                self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2  
-
-                #neuron o1
-                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+                # Neuron h2
+                self.h2.weights[0] -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
+                self.h2.weights[1] -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
+                self.h2.bias -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
 
                 #calculate the total loss at the end of each epoch
-                if epoch % 100 == 0:
-                    y_preds = np.apply_along_axis(self.feedforward, 1, data)
-                    loss = mse_loss(all_y_trues, y_preds)
-                    print("Epoch %d loss: %.3f" % (epoch, loss))
-
+            if epoch % 100 == 0:
+                y_preds = np.apply_along_axis(self.feedforward, 1, data)
+                loss = mse_loss(all_y_trues, y_preds)
+                print("Epoch %d loss: %.3f" % (epoch, loss))
+     
 #define dataset
 data = np.array([
     [-2,-1],    #alice
