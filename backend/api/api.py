b8a3ed7732518975eb249fbde94f88e24fe7d81d
nashawe <nathaniel.shawe@gmail.com>
2025-05-19 13:42:30 +0200

    backend deployment

--- backend/api/api.py
+++ backend/api/api.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # or set to frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
